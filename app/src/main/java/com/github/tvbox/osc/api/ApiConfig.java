ee4d07cf96a1f863563dcb5dbd41051ef826b2b2
戈小戈 <51260376+GXG12345@users.noreply.github.com>
2023-06-16 23:56:58 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://jihulab.com/custom2/tv/-/raw/main/gxgs.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
